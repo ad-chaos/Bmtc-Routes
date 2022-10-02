46c7c73722c7343a6c8ee4d68c8c67443f0a7c44
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-10-02 19:26:14 +0530

    Added pictures to replace catto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:collection/collection.dart';
+import 'package:tuple/tuple.dart';
 
 void main() {
   runApp(const BmtcApp());
@@ -61,9 +61,9 @@ class _LoginScreenState extends State<LoginScreen> {
   TextFormField passwordField = TextFormField(
     obscureText: true,
     decoration: const InputDecoration(
-      border: UnderlineInputBorder(),
-      labelText: 'Password',
-    ),
+        border: UnderlineInputBorder(),
+        labelText: 'Password',
+        labelStyle: TextStyle(fontSize: 20)),
     validator: (input) {
       if (input == "") {
         return "Can't be null mate :)";
@@ -75,9 +75,9 @@ class _LoginScreenState extends State<LoginScreen> {
   );
   TextFormField usernameField = TextFormField(
     decoration: const InputDecoration(
-      border: UnderlineInputBorder(),
-      labelText: 'Username',
-    ),
+        border: UnderlineInputBorder(),
+        labelText: 'Username',
+        labelStyle: TextStyle(fontSize: 20)),
     validator: (input) {
       if (input == "") {
         return "You Sure you don't have a name?";
@@ -95,24 +95,31 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Center(
           child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
             Container(
-                constraints: const BoxConstraints(maxWidth: 500),
+                constraints: const BoxConstraints(maxWidth: 300),
                 child: usernameField),
             Container(
-                constraints: const BoxConstraints(maxWidth: 500),
+                constraints: const BoxConstraints(maxWidth: 300),
                 child: passwordField),
-            Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formkey.currentState!.validate()) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const HomeScreen()));
-                    }
-                  },
-                  child: const Text('Login'),
-                )),
+            SizedBox(
+              height: 70,
+              width: 200,
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomeScreen()));
+                      }
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  )),
+            ),
           ]),
         ),
       ),
@@ -125,40 +132,32 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> iconsName = [
-      "TripPlanner.jpeg",
-      "RouteMap.jpeg",
-      "fare.jpeg",
-      "bus.jpeg",
-      "live.jpeg",
-      "recharge.jpeg"
-    ];
-    List<String> buttonsName = [
-      "Trip Planner",
-      "Route Map",
-      "Fare",
-      "Nearby Bus Stop",
-      "Live Tracking",
-      "Recharge/Bus Pass"
+    const homeScreen = <Tuple3<String, String, StatelessWidget>>[
+      Tuple3("TripPlanner.jpeg", "Trip Planner", TripPlanner()),
+      Tuple3("RouteMap.jpeg", "Route Map", RouteFinder()),
+      Tuple3("fare.jpeg", "Fare", Fare()),
+      Tuple3("bus.jpeg", "Nearby Bus Stop", BusStop()),
+      Tuple3("live.jpeg", "Live Tracking", Live()),
+      Tuple3("recharge.jpeg", "Recharge/Bus Pass", Recharge())
     ];
     List<Padding> icons = <Padding>[];
     List<Padding> buttons = <Padding>[];
-    for (final iButton in IterableZip([iconsName, buttonsName])) {
+    for (final iButton in homeScreen) {
       icons.add(Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Image.asset(iButton[0], width: 75, height: 75)));
+          child: Image.asset(iButton.item1, width: 75, height: 75)));
       buttons.add(Padding(
         padding: const EdgeInsets.all(25),
         child: SizedBox(
             height: 50,
             child: ElevatedButton(
                 child: Text(
-                  iButton[1],
-                  style: const TextStyle(fontSize: 25),
+                  iButton.item2,
+                  style: const TextStyle(fontSize: 18),
                 ),
                 onPressed: () {
                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const Catto()));
+                      MaterialPageRoute(builder: (context) => iButton.item3));
                 })),
       ));
     }
@@ -203,3 +202,70 @@ class Catto extends StatelessWidget {
         body: Center(child: Image.asset('opening.png')));
   }
 }
+
+class TripPlanner extends StatelessWidget {
+  const TripPlanner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("TripPlanner")),
+        body: Center(child: Image.asset('TripPlannerWidget.png')));
+  }
+}
+
+class RouteFinder extends StatelessWidget {
+  const RouteFinder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("RouteFinder")),
+        body: Center(child: Image.asset('RouteFinderWidget.png')));
+  }
+}
+
+class Fare extends StatelessWidget {
+  const Fare({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Fare")),
+        body: Center(child: Image.asset('FareWidget.png')));
+  }
+}
+
+class BusStop extends StatelessWidget {
+  const BusStop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Nearby Bus Stops")),
+        body: Center(child: Image.asset('BusStopWidget.png')));
+  }
+}
+
+class Live extends StatelessWidget {
+  const Live({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Live Tracking")),
+        body: Center(child: Image.asset('LiveWidget.png')));
+  }
+}
+
+class Recharge extends StatelessWidget {
+  const Recharge({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Recharge and Bus Passes")),
+        body: Center(child: Image.asset('RechargeWidget.png')));
+  }
+}
+
